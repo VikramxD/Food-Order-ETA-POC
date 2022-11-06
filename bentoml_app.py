41ab6ddc0eb270da9919940d71f3c520c5aa994f
Vikram <vikramblaze1782000@gmail.com>
2022-11-06 17:24:23 +0530

    💄 UI: App init

--- bentoml_app.py
+++ bentoml_app.py
@@ -1,18 +1,24 @@
 import bentoml
 import bentoml.sklearn
-from bentoml.io import NumpyNdarray, PandasDataFrame
-
+from bentoml.io import NumpyNdarray,PandasDataFrame
+from pydantic import BaseModel
 import pickle
 import numpy as np
 import pandas as pd
-
 predictor = bentoml.sklearn.get("orderpredictor:lcjrxys4ewkvo6ty").to_runner()
 service = bentoml.Service(
-    "OrderPrediction", runners=[predictor]
+    "order_prediction", runners=[predictor]
 )
+
+
+
+
+
+
 
 
 @service.api(input=NumpyNdarray(),output=NumpyNdarray())
 def predict(input: np.ndarray) -> np.ndarray:
     result = predictor.run(input)
     return np.array(result)
+
--- src/scripts/inference.py
+++
@@ -1,3 +0,0 @@
-import pickle 
-
-pickle.load('/Users/vicariousvision/Analytics Stuff/amazon_ml_challenge/src/models/model.pkl')
